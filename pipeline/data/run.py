be2beb69835af99d7b37d05f3a90236292ee8669
HabeebShopeju <shopejuh@gmail.com>
2025-10-03 10:01:00 +0100

    Fixed sampling for English dataset

--- pipeline/data/run.py
+++ pipeline/data/run.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import math
 
 import jsonlines
 
@@ -14,6 +15,8 @@ from pipeline.data.translate import make_english_dataset
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+EVAL_TEST_ROW_COUNT = 2000
 
 def main(args):
     language = args.language.lower()
@@ -51,7 +54,7 @@ def main(args):
         filepath = f"{ARTEFACTS_DIR}/filtered_{args.language}_{args.split}_dataset.jsonl"
         with jsonlines.open(filepath, "w") as f_:
             if DataSplit.test in filepath or DataSplit.eval in filepath:
-                f_.write_all(sample_data(filtered_lines, n=2000))
+                f_.write_all(sample_data(filtered_lines, n=EVAL_TEST_ROW_COUNT))
             else:
                 f_.write_all(filtered_lines)
         logger.info(f"Filtered dataset created for {args.language} and saved to {filepath}.")
@@ -63,7 +66,29 @@ def main(args):
         filepath = f"{ARTEFACTS_DIR}/filtered_{args.language}_{args.split}_dataset.jsonl"
         with jsonlines.open(filepath, "w") as f_:
             if DataSplit.test in filepath or DataSplit.eval in filepath:
-                f_.write_all(sample_data(rows, n=2000))
+                lang_to_idx = {
+                    Language.yoruba: 0,
+                    Language.igbo: 1,
+                    Language.hausa: 2
+                }
+                language_rows = [[], [], []]
+                language_row_counts = [0, 0, 0]
+
+                for row in rows:
+                    language_rows[lang_to_idx[row["root_query_language"]]].append(row)
+                    language_row_counts[lang_to_idx[row["root_query_language"]]] += 1
+
+                sorted_indices = sorted(range(len(language_rows)), key=lambda x: language_row_counts[x])
+                # Attempt to get a balanced sample per language
+                sampled_rows = []
+                for idx, lang_idx in enumerate(sorted_indices):
+                    remaining_rows = EVAL_TEST_ROW_COUNT - len(sampled_rows)
+                    desired_minimum_rows = math.ceil(remaining_rows / (len(sorted_indices) - idx))
+                    if language_row_counts[lang_idx] <= desired_minimum_rows:
+                        sampled_rows.extend(language_rows[lang_idx])
+                    else:
+                        sampled_rows.extend(sample_data(language_rows[lang_idx], n=desired_minimum_rows))
+                f_.write_all(sampled_rows)
             else:
                 f_.write_all(rows)
         logger.info(f"Filtered dataset translated for {args.language} and saved to {filepath}.")
